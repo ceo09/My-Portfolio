79963a99f1c080fbf7b000336078a1efde6f8c94
courage okpa <ceocoetta2009@gmail.com>
2023-03-12 20:13:27 -0700

    changed some things

---
+++ AI.js
@@ -0,0 +1,11 @@
+/*let linkedIn = document.querySelector('#linked')
+
+linkedIn.addEventListener('click', () => {
+
+    swal({
+        title: "Are you sure you want to leave this page",
+        text: "Leaving this page will take you to another page.",
+        icon: "info",
+        button: "YES"
+    })
+})*/
